2f03eecff895ab428731a863a0274643a0c30819
Paul Mineiro <paul.mineiro@gmail.com>
2023-09-29 13:49:02 +0000

    more cleanups

--- personalized_citation/TaskLLM.py
+++ personalized_citation/TaskLLM.py
@@ -14,7 +14,7 @@ class TaskLLM(torch.nn.Module):
         self._decoder_input_ids = self._tokenizer(["["], return_tensors='pt').input_ids
         self._one, self._two = self._tokenizer(["1"]).input_ids[0][0], self._tokenizer(["2"]).input_ids[0][0]
 
-    def forward(self, data, *, augment = None):
+    def forward(self, data, *, augment=None):
         import torch.nn.functional as F
 
         enc = self._tokenizer(data, return_tensors='pt', padding=True).to(self._transformer.device)
@@ -23,43 +23,24 @@ class TaskLLM(torch.nn.Module):
         logprobs = F.log_softmax(logits, dim=1)
 
         if augment:
-            augment_logprobs = self.forward(augment(data)) 
-            return torch.logaddexp(logprobs, augment_logprobs) - torch.log(2)
+            from math import log
+            augment_logprobs = self.forward(augment(data))[:,[1,0]]
+            return torch.logaddexp(logprobs, augment_logprobs) - log(2)
         else:
             return logprobs
 
-    # TODO: doesn't work with custom T5
-    #def clone(self):
-    #    other = T5Classifier(self._num_labels, opt_factory=self._opt_factory)
-    #    other.load_state_dict(self.state_dict())
-    #    other._optim = other._opt_factory(other.parameters())
-    #    other._optim.load_state_dict(self._optim.state_dict())
-    #    return other
-
-    def predict(self, x, *, augment = None):
+    def predict(self, x, *, augment=None):
         self.eval()
         return self.forward(x, augment = augment)
 
-    def learn(self, x, y, *, augment = None):
+    def learn(self, x, y, *, augment=None):
         import torch.nn.functional as F
 
         # self.train() gives bad results ... (?)
         self.eval()
         self._optim.zero_grad()
         output = self(x, augment = augment)
-        loss = F.nll_loss(output, y) 
-        loss.backward()
-        self._optim.step()
-        return loss.item()
-
-    def bandit_learn(self, x, a, r, augment = None):
-        import torch.nn.functional as F
-
-        self.eval()
-        self._optim.zero_grad()
-        logprobs = self(x, augment = augment)
-        indexed_logprobs = logprobs[range(logprobs.shape[0]), a]
-        loss = F.binary_cross_entropy(torch.exp(indexed_logprobs), r)
+        loss = F.nll_loss(output, y)
         loss.backward()
         self._optim.step()
         return loss.item()
@@ -69,10 +50,10 @@ class TaskLLM(torch.nn.Module):
         self._tokenizer.save_pretrained(model_id)
 
 class PeftTaskLLM(TaskLLM):
-    def __init__(self, num_labels, peft_config, *, t5=None, opt_factory=None, model_id=None):
+    def __init__(self, peft_config, *, t5=None, opt_factory=None, model_id=None):
         from peft import get_peft_model, PeftModel
 
-        super().__init__(num_labels, t5=t5, opt_factory=opt_factory) # NB: no model_id here
+        super().__init__(t5=t5, opt_factory=opt_factory) # NB: no model_id here
         self._peft_config = peft_config
         if model_id is None:
             self._transformer = get_peft_model(self._transformer, self._peft_config)
@@ -81,9 +62,8 @@ class PeftTaskLLM(TaskLLM):
         self._optim = self._opt_factory(self.parameters())
 
     def clone(self):
-        other = PeftT5Classifier(self._num_labels, self._peft_config, t5=self._transformer.base_model.model)
+        other = TaskLLM(self._peft_config, t5=self._transformer.base_model.model, opt_factory=self._opt_factory)
         other._transformer.load_state_dict(self._transformer.state_dict())
-        other._score.load_state_dict(self._score.state_dict())
         other._optim = self._opt_factory(other.parameters())
         other._optim.load_state_dict(self._optim.state_dict())
 
