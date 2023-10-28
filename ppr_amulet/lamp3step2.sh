3e47b2920242a0b8665222dfe988b53876d49d09
Paul Mineiro <paul.mineiro@gmail.com>
2023-10-28 15:41:35 +0000

    lamp 3 amulet stuff

--- ppr_amulet/lamp3step2.sh
+++ ppr_amulet/lamp3step2.sh
@@ -2,4 +2,4 @@ cd personalized_product_rating
 cp /mnt/default/uploads/data/personalized_product_rating/*.json.gz .
 cp -a /mnt/default/uploads/data/personalized_product_rating/User_keq4_t5base_step1_iter* .
 (mkdir -p ~/.cache; cp -a /mnt/default/uploads/cache/* ~/.cache/)
-env AUGMENT=$1 python ../runnbcell.py -y User_keq4_t5base-loraaugtrunc.ipynb 1 "$AMLT_OUTPUT_DIR"/User_keq4_t5base-loraaugtrunc.ipynb
+env AUGMENT=$1 GAMMA=$2 python ../runnbcell.py -y User_keq4_t5base.ipynb 1 "$AMLT_OUTPUT_DIR"/User_keq4_t5base.ipynb
