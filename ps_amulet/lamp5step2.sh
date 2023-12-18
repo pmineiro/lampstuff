2a107ce039cb1797129832a1f157aa99aad34d35
Paul Mineiro <paul.mineiro@gmail.com>
2023-12-18 19:08:04 +0000

    lamp6

--- ps_amulet/lamp5step2.sh
+++
@@ -1,5 +0,0 @@
-cd personalized_scholarly
-cp /mnt/default/uploads/data/personalized_scholarly/*.json.gz .
-cp -a /mnt/default/uploads/data/personalized_scholarly/User_keq4_t5base_step1_iter* .
-(mkdir -p ~/.cache; cp -a /mnt/default/uploads/cache/* ~/.cache/)
-env AUGMENT=$1 GAMMA=$2 python ../runnbcell.py -y User_keq4_t5base.ipynb 1 "$AMLT_OUTPUT_DIR"/User_keq4_t5base.ipynb
